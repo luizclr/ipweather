1ded6eef6036968fbc3d83efeefde94f280bff74
Luiz Cláudio <luiz.lclr@gmail.com>
2020-01-28 16:49:17 -0300

    Configure page position

---
+++ src/components/info/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../../styles/components/info/index.scss";
+
+const Info = () => (
+  <div className="info">
+    <h1>Info</h1>
+  </div>
+);
+
+export default Info;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Info from "../../components/info";
 
-const Home = () => <h1>Home</h1>;
+import "../../styles/pages/home/index.scss";
+
+const Home = () => (
+  <div className="home">
+    <div className="home-container">
+      <h1>Map</h1>
+    </div>
+    <div className="home-container">
+      <Info />
+    </div>
+  </div>
+);
 
 export default Home;
